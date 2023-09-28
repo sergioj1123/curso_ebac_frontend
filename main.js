4d6f2213188ef7b60e326ae939750ebba685e3ff
Sergio Felix <sergiofelix9@gmail.com>
2023-09-27 21:02:49 -0300

    exercio es6

--- main.js
+++ main.js
@@ -1,65 +1,20 @@
-class Client {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+const todosAlunos = [];
+
+todosAlunos.push({ name: "Sergio", nota: 10 });
+todosAlunos.push({ name: "Aluno A", nota: 9 });
+todosAlunos.push({ name: "Aluno B", nota: 8 });
+todosAlunos.push({ name: "Aluno C", nota: 6 });
+todosAlunos.push({ name: "Aluno D", nota: 5 });
+todosAlunos.push({ name: "Aluno E", nota: 0 });
+
+console.log("Todos alunos: ");
+console.log(todosAlunos);
+
+const aprovados = todosAlunos.map((item) => {
+  if (item.nota >= 6) {
+    return item;
   }
-  _getName() {
-    console.log(`The client name is: ${this.name}`);
-  }
-}
-
-class NormalAccount extends Client {
-  constructor(name, age, balance, accountNumber) {
-    super(name, age);
-    this._balance = balance;
-    this._accountNumber = accountNumber;
-    this._accountType = "normalAccount";
-  }
-
-  drawBalance(value) {
-    if (this._balance >= value) {
-      this._balance -= value;
-      return value;
-    }
-  }
-
-  deposit(value) {
-    this._balance += value;
-    return this._balance;
-  }
-
-  getBalance() {
-    return console.log(`O saldo da conta é: ${this._balance}`);
-  }
-}
-
-class SavingAccount extends NormalAccount {
-  constructor(name, age, balance, accountNumber) {
-    super(name, age, balance, accountNumber);
-    this._yield = 1.2;
-    this._accountType = "Saving Account";
-  }
-
-  newBalanceAfterOneMonth() {
-    return (this._balance *= this._yield);
-  }
-}
-
-console.log("----------------------------------------------------------------");
-const normalAccount = new NormalAccount("Sergio", 25, 500, 123);
-normalAccount._getName();
-normalAccount.getBalance();
-console.log("🚀 ~ file: main.js:49 ~ normalAccount:", normalAccount);
-
-console.log("----------------------------------------------------------------");
-const saving = new SavingAccount("Test", 25, 1000, 321);
-saving._getName();
-saving.newBalanceAfterOneMonth();
-saving.getBalance();
-console.log("🚀 ~ file: main.js:52 ~ saving:", saving);
-
-console.log("----------------------------------------------------------------");
-const saving2 = new SavingAccount("Test 3", 25, 1000, 111111);
-saving2._getName();
-saving2.getBalance();
-console.log("🚀 ~ file: main.js:57 ~ saving2:", saving2);
+});
+console.log("--------------------------------");
+console.log("Aprovados");
+console.log(aprovados);
