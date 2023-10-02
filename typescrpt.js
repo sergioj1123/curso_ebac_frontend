f4f6a602ccf0775ae94f8619a17adc5a8297bcd8
Sergio Felix <sergiofelix9@gmail.com>
2023-10-02 16:34:13 -0300

    exercio typescrtipt

---
+++ typescrpt.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+function sawHello(name) {
+    return `Olá ${name}`;
+}
+console.log(multiply(10, 2));
+console.log(sawHello("Sérgio"));
---
+++ typescrpt.ts
@@ -0,0 +1,10 @@
+function multiply (a: number, b: number): number{
+    return a*b;
+}
+
+function sawHello (name: string): string {
+    return `Olá ${name}`;
+}
+
+console.log(multiply(10, 2));
+console.log(sawHello("Sérgio"));
